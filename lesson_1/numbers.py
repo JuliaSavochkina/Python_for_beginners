e4e7c4b7ed36ff1cf85896a3d640bbd766e1cbca
Kseniya <stranjerry21@gmail.com>
2022-06-04 14:15:14 +0300

    changed back

--- lesson_1/numbers.py
+++ lesson_1/numbers.py
@@ -1,41 +1,18 @@
 """int, float"""
 
 # Выберите два числа и возведите одно в степень другого двумя способами
-# 1 способ:
-x = 10
-y = 3
-print(x**y)
 
-# 2 способ:
-a = 5
-b = 3
-import math
-f = math.pow (5, 3)
-print(f)
 
 
 # Переведите число x = 0.73 в шестнадцатиричное
-x = 0.73
-b = (x).hex()
-print(b)
+
 
 
 # Для чисел 3.0, 8.5, 5.66 узнайте, какое из них целое
 
-str = (3.0).is_integer()
-print(str)
-
-str = (8.5).is_integer()
-print(str)
-
-str = (5.66).is_integer()
-print(str)
 
 
 
 # переведите букву l в число ASCII
 
-str = "l"
-result = ord(str)
-print(result)
 
--- lesson_1/strings.py
+++ lesson_1/strings.py
@@ -5,35 +5,19 @@
 s = '  svGHytr  TYUijthHG  '
 
 # Подсчитайте количество символов t в строке
-s = '  svGHytr  TYUijthHG  '
-print(s.count('t'))
 
 
 # Проверьте входит ли подстрока 'tyu' в строку
-s = '  svGHytr  TYUijthHG  '
-result = 'tyu' in s
-print(result)
-
 
 
 # Проверьте, состоит ли строка только из заглавных букв
-s = '  svGHytr  TYUijthHG  '
-result = s. isupper()
-print(result)
+
 
 
 # Удалите пробелы до и после слов строки
 
-s = '  svGHytr  TYUijthHG  '
-print(s.strip())
-
-
 
 # * Удалите все пробелы из строки и сделайте первую букву заглавной
-
-s = '  svGHytr  TYUijthHG  '
-x = s.replace(' ', "")
-print(x.capitalize())
 
 
 
@@ -45,9 +29,4 @@ print(x.capitalize())
 # ** Cложите строку «5» и число 5. Чтобы на выходе получилось число. Не используя операция int().
 
 
-a = '5'
-b = 5
-f = (repr(b))
-res_str = str(f) + a
-print(res_str)
 
