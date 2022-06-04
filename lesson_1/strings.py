751e2baa30df9ecbc2c9504a4eac528de5d28145
Kseniya <stranjerry21@gmail.com>
2022-06-04 13:45:14 +0300

    Added solutions to "strings.py"

--- lesson_1/strings.py
+++ lesson_1/strings.py
@@ -5,20 +5,49 @@
 s = '  svGHytr  TYUijthHG  '
 
 # Подсчитайте количество символов t в строке
-# напишите решение здесь
+s = '  svGHytr  TYUijthHG  '
+print(s.count('t'))
+
 
 # Проверьте входит ли подстрока 'tyu' в строку
+s = '  svGHytr  TYUijthHG  '
+result = 'tyu' in s
+print(result)
+
 
 
 # Проверьте, состоит ли строка только из заглавных букв
+s = '  svGHytr  TYUijthHG  '
+result = s. isupper()
+print(result)
 
 
 # Удалите пробелы до и после слов строки
 
+s = '  svGHytr  TYUijthHG  '
+print(s.strip())
+
+
 
 # * Удалите все пробелы из строки и сделайте первую букву заглавной
+
+s = '  svGHytr  TYUijthHG  '
+x = s.replace(' ', "")
+print(x.capitalize())
+
+
 
 
 # ** Дана строчная латинская буква, переведите ее в заглавную без использования втрокового метода
 
+
+
 # ** Cложите строку «5» и число 5. Чтобы на выходе получилось число. Не используя операция int().
+
+
+a = '5'
+b = 5
+f = (repr(b))
+res_str = str(f) + a
+print(res_str)
+
