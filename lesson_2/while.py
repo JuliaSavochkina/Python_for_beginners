ba41d90ce431e5bf2ac8002a6a6e931b49267f23
Anastasia_Matveeva <nastyamatveyeva@gmail.com>
2022-06-18 09:56:29 +0300

    added answers to 2 lesson

--- lesson_2/while.py
+++ lesson_2/while.py
@@ -1,22 +1,20 @@
 """
 Пользователь вводит положительные целые числа до тех пор, пока не введет 0. Выведите максимальное введенное число (0 считать не нужно)."""
-
-count = 0
-list = []
-while True:
-    n = int(input('Введите число: '))
-    count += 1
-    list.append(n)
-    if n == 0:
+number = int(input('Введите число:'))
+while number != 0:
+    number = int(input('Введите число:'))
+    if number == 0:
         break
-print(max(list))
+        print(number)
+
+
 
 """* Пользователь вводит число N. Выведите факториал число N. Факториал числа N - это произведение всех чисел от
 1 до N включительно. Например, факториал числа 5 равен 120.
 """
-number = int(input('Введите число: '))
+n = int(input('Введите число для вычисления факториала:'))
 factorial = 1
-while number > 1:
-    factorial = factorial * number
-    number = number - 1
-print(factorial)
+while n > 1:
+    factorial = factorial * n
+    n = n - 1
+    print(factorial)
