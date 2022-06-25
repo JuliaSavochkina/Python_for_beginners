78997697c34001822b9306e1a8c68ab57cc6a933
Julia Savochkina <j.savochkina@admitad.com>
2022-06-25 16:42:14 +0300

    added lesson 3

---
+++ lesson_3/func.py
@@ -0,0 +1,18 @@
+"""
+* Напишите функции для методов строк через таблицу ASCII
+- isdigit()
+- islower()
+- isalnum()
+"""
+
+
+def isdigit(a):
+    pass
+
+
+def islower(a):
+    pass
+
+
+def isalnum(a):
+    pass
